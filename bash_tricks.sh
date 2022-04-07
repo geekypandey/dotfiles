3cd000a3097f9429a030a1683b87998989f5a846
Anurag Pandey <anuragcarbon1998@gmail.com>
2022-04-07 20:59:32 +0530

    add bash tricks
    some useful aliases and functions

---
+++ bash_tricks.sh
@@ -0,0 +1,26 @@
+# common mistypes
+alias sl='ls'
+alias dc='cd'
+alias iv='vi'
+
+alias ..='cd ..'
+alias ....='cd ../../'
+
+
+# run tests
+alias ut='python -m unittest'
+
+
+# create and move into that directory
+function md() {
+	mkdir $1 && cd $1;
+}
+
+# borrowed from https://github.com/nibalizer/bash-tricks/blob/master/bash_tricks.sh
+function ,cdp() {
+	TEMP_PWD=$(pwd)
+	while ! [ -d .git ]; do
+		cd ..
+	done
+	OLDPWD=$TEMP_PWD
+}
